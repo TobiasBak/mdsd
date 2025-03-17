872d9aee42934e42719d3f08f03e2cc8f093b757
TobiasBak <tobib21@student.sdu.dk>
2025-03-17 09:22:55 +0100

    Modified validation example to us our model

--- langium/er-dsl/src/language/goat-jh-validator.ts
+++ langium/er-dsl/src/language/goat-jh-validator.ts
@@ -1,5 +1,5 @@
-import type { ValidationChecks } from 'langium';
-import type { GoatJhAstType } from './generated/ast.js';
+import type { ValidationChecks, ValidationAcceptor } from 'langium';
+import type { Entity, GoatJhAstType } from './generated/ast.js';
 import type { GoatJhServices } from './goat-jh-module.js';
 
 /**
@@ -9,6 +9,7 @@ export function registerValidationChecks(services: GoatJhServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.GoatJhValidator;
     const checks: ValidationChecks<GoatJhAstType> = {
+        // Entity: (entity, accept) => validator.checkPersonStartsWithCapital(entity, accept)
         
     };
     registry.register(checks, validator);
@@ -19,13 +20,13 @@ export function registerValidationChecks(services: GoatJhServices) {
  */
 export class GoatJhValidator {
 
-    //checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
-    //    if (person.name) {
-    //        const firstChar = person.name.substring(0, 1);
+    // checkPersonStartsWithCapital(entity: Entity, accept: ValidationAcceptor): void {
+    //    if (entity.name) {
+    //        const firstChar = entity.name.substring(0, 1);
     //        if (firstChar.toUpperCase() !== firstChar) {
-    //            accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+    //            accept('warning', 'Entity name should start with a capital.', { node: entity, property: 'name' });
     //        }
     //    }
-    //}
+    // }
 
 }
